eab93c993de7cae326773f79b40dcb3f44831dae
Sora210 <46365745+Sora-210@users.noreply.github.com>
2023-09-19 07:02:35 +0000

    update: 住所関連のmigration/modelを定義

--- rails/db/migrate/20230919063951_create_cities.rb
+++ rails/db/migrate/20230919063951_create_cities.rb
@@ -1,8 +1,10 @@
 class CreateCities < ActiveRecord::Migration[7.0]
   def change
     create_table :cities do |t|
+      t.string :name, null: false
+      t.string :yomi, null: false
 
-      t.timestamps
+      t.references :prefectures, forgin_key: true
     end
   end
 end
