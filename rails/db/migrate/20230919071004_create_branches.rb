e058e7d9c38e9dc1fb40bb395257066c41c2e18b
Sora210 <46365745+Sora-210@users.noreply.github.com>
2023-09-20 01:55:35 +0000

    fix: カラム名の修正

--- rails/db/migrate/20230919071004_create_branches.rb
+++ rails/db/migrate/20230919071004_create_branches.rb
@@ -8,10 +8,10 @@ class CreateBranches < ActiveRecord::Migration[7.0]
       t.string :logo_url, null: false
       t.string :post_code, null: false
       t.string :address, null: false
-      t.string :phone
+      t.string :tel
       t.string :fax
       t.string :business_hours_text, comment: '営業時間 <ex: 平日9:00-18:00>'
-      t.string :holidays, comment: '<ex: 水曜日、祝日>'
+      t.string :holidays_text, comment: '<ex: 水曜日、祝日>'
       t.string :catchcopy, null: false
       t.text :introduction, null: false
       t.timestamps
