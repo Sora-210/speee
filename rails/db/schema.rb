44ef4bb5e6aeb645a772751bdc49833f0220d739
Sora210 <46365745+Sora-210@users.noreply.github.com>
2023-09-20 02:08:32 +0000

    fix: schemaの修正

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -28,10 +28,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_082752) do
     t.string "logo_url", null: false
     t.string "post_code", null: false
     t.string "address", null: false
-    t.string "phone"
+    t.string "tel"
     t.string "fax"
     t.string "business_hours_text", comment: "営業時間 <ex: 平日9:00-18:00>"
-    t.string "holidays", comment: "<ex: 水曜日、祝日>"
+    t.string "holidays_text", comment: "<ex: 水曜日、祝日>"
     t.string "catchcopy", null: false
     t.text "introduction", null: false
     t.datetime "created_at", null: false
