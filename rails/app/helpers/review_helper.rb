6ea08280b60082aa935c19a64a7005d5c524a78a
ryota108 <83957178+ryota108@users.noreply.github.com>
2023-09-21 05:23:33 +0000

    ヘルパー関数への切り分け、display_sale_reason関数の修正

--- rails/app/helpers/review_helper.rb
+++ rails/app/helpers/review_helper.rb
@@ -1,17 +1,10 @@
 # app/helpers/reviews_helper.rb
 module ReviewHelper
-    def display_sale_reason(sale_reason_type)
-      reasons = {
-        "moving_house" => "住み替え",
-        "inheritance" => "相続",
-        "transfer_job" => "転勤、転職",
-        "divorce" => "離婚",
-        "assets" => "資産整理",
-        "money" => "金銭的な理由",
-        "other" => "その他"
-      }
-  
-      reasons[sale_reason_type]
+  def display_sale_reason(sale_reason_type)
+    I18n.t("activerecord.attributes.review.sale_reasons.#{sale_reason_type}")
+  end
+    def format_japanese_date(date)
+      date.strftime("%Y年%-m月")
     end
   end
   
