8015edffa665b76fca96e73cf92217382a8724b5
takano raika <62830507+Kissa0330@users.noreply.github.com>
2023-09-21 08:39:28 +0000

    formの原型

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   mount KomachiHeartbeat::Engine => '/ops'
   root 'home#index'
+  get '/assess', to: 'assessment#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
