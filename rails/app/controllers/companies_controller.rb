3832aea23c58c5a3e0e2e7bc0b51e2a9226f8263
ryota108 <83957178+ryota108@users.noreply.github.com>
2023-09-22 06:59:17 +0000

    averageの追加と表示

--- rails/app/controllers/companies_controller.rb
+++ rails/app/controllers/companies_controller.rb
@@ -2,7 +2,10 @@ class CompaniesController < ApplicationController
     def show
       @branch = Branch.includes(:prefecture, :city, :reviews).find(params[:branch_id])
       @company = @branch.company
-      # 必要に応じて他の処理を追加
+      @speed_cs_avg = @branch.reviews.average(:speed_cs)
+      @price_cs_avg = @branch.reviews.average(:price_cs)
+      @support_cs_avg = @branch.reviews.average(:support_cs)
+      @overall_avg = (@speed_cs_avg + @price_cs_avg + @support_cs_avg) / 3.0
     end
   end
   
--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+    def rounded_avg(value)
+        value.round(2)
+    end
 end
