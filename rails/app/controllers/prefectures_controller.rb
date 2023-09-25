363c583bcfc522adc53ea1de20ebce7eb51efb47
takano raika <62830507+Kissa0330@users.noreply.github.com>
2023-09-25 09:13:02 +0000

    fix conflict

--- rails/app/controllers/prefectures_controller.rb
+++ rails/app/controllers/prefectures_controller.rb
@@ -1,14 +1,18 @@
 class PrefecturesController < ApplicationController
     def index
-        @prefecture = Prefecture.find(params[:id])
-        @branches = Branch.where(prefecture_id: params[:id])
-        @cities = City.where(prefecture_id: params[:id])
-        @branch_reviews = []
-        @average_score = []
-        @branches.each do |branch|
-            @target_reviews = Review.where(branch_id: branch.id)
-            @average_score = (@target_reviews.average(:speed_cs) + @target_reviews.average(:price_cs) + @target_reviews.average(:support_cs)) / 3
-            @branch_reviews.push(@target_reviews)
+        @prefecture = Prefecture.where(name: params[:name]).first
+        if @prefecture.present?
+            @branches = Branch.where(prefecture_id: @prefecture.id)
+            @cities = City.where(prefecture_id: @prefecture.id)
+            @branch_reviews = []
+            @average_score = []
+            @branches.each do |branch|
+                @target_reviews = Review.where(branch_id: branch.id)
+                @average_score = (@target_reviews.average(:speed_cs) + @target_reviews.average(:price_cs) + @target_reviews.average(:support_cs)) / 3
+                @branch_reviews.push(@target_reviews)
+            end
+        else
+            redirect_to "/error"
         end
     end
 end
