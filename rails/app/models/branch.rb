7c34c95994cdb1dac0960b11b822ec70de21e195
takano raika <62830507+Kissa0330@users.noreply.github.com>
2023-09-25 08:53:54 +0000

    branchClassにaverage_scoreメソッドの実装

--- rails/app/models/branch.rb
+++ rails/app/models/branch.rb
@@ -5,4 +5,13 @@ class Branch < ApplicationRecord
     has_many :assesment_areas
     has_many :reviews
     has_many :assesments
+    def average_score
+        target_reviews = reviews
+        total_score = 0
+        total_score += target_reviews.average(:speed_cs).to_f
+        total_score += target_reviews.average(:price_cs).to_f
+        total_score += target_reviews.average(:support_cs).to_f
+        total_score /= 3.0
+        return total_score.round(1)
+      end
 end
