69932ecdfaa6072d143a96723b9818d7461f8c0e
takano raika <62830507+Kissa0330@users.noreply.github.com>
2023-09-25 09:10:00 +0000

    微修正

--- rails/app/models/branch.rb
+++ rails/app/models/branch.rb
@@ -7,21 +7,18 @@ class Branch < ApplicationRecord
     has_many :assesments
     def all_average_score
         total_score = (speed_average_score + price_average_score + support_cs_average_score) / 3
-        return total_score.round(1)
+        total_score.round(1)
       end
       def speed_average_score
-        target_reviews = reviews
-        score = target_reviews.average(:speed_cs).to_f
-        return score.round(1)
+        score = reviews.average(:speed_cs).to_f
+        score.round(1)
       end
       def price_average_score
-        target_reviews = reviews
-        score = target_reviews.average(:price_cs).to_f
-        return score.round(1)
+        score = reviews.average(:price_cs).to_f
+        score.round(1)
       end
       def support_average_score
-        target_reviews = reviews
-        score = target_reviews.average(:support_cs).to_f
-        return score.round(1)
+        score = reviews.average(:support_cs).to_f
+        score.round(1)
       end
 end
