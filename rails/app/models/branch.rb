5f5498a6a0b1eadfbf4a2fa0ff1238f3bea9b5fa
takano raika <62830507+Kissa0330@users.noreply.github.com>
2023-09-25 09:01:37 +0000

    scoreの種類毎に生やす

--- rails/app/models/branch.rb
+++ rails/app/models/branch.rb
@@ -5,13 +5,23 @@ class Branch < ApplicationRecord
     has_many :assesment_areas
     has_many :reviews
     has_many :assesments
-    def average_score
-        target_reviews = reviews
-        total_score = 0
-        total_score += target_reviews.average(:speed_cs).to_f
-        total_score += target_reviews.average(:price_cs).to_f
-        total_score += target_reviews.average(:support_cs).to_f
-        total_score /= 3.0
+    def all_average_score
+        total_score = (speed_average_score + price_average_score + support_cs_average_score) / 3
         return total_score.round(1)
+      end
+      def speed_average_score
+        target_reviews = reviews
+        score = target_reviews.average(:speed_cs).to_f
+        return score.round(1)
+      end
+      def price_average_score
+        target_reviews = reviews
+        score = target_reviews.average(:price_cs).to_f
+        return score.round(1)
+      end
+      def support_average_score
+        target_reviews = reviews
+        score = target_reviews.average(:support_cs).to_f
+        return score.round(1)
       end
 end
