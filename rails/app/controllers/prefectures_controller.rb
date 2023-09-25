7b65c8000816c75967dab054cbdba79a703dd893
takano raika <62830507+Kissa0330@users.noreply.github.com>
2023-09-25 09:54:20 +0000

    リファクタリング

--- rails/app/controllers/prefectures_controller.rb
+++ rails/app/controllers/prefectures_controller.rb
@@ -6,12 +6,5 @@ class PrefecturesController < ApplicationController
         end
             @branches = Branch.where(prefecture_id: @prefecture.id)
             @cities = City.where(prefecture_id: @prefecture.id)
-            @branch_reviews = []
-            @average_score = []
-            @branches.each do |branch|
-                @target_reviews = Review.where(branch_id: branch.id)
-                @average_score = branch.all_average_score
-                @branch_reviews.push(@target_reviews)
-            end
     end
 end
