3cf8c7bf0ba117027e64266cddd46da68719bfaf
takano raika <62830507+Kissa0330@users.noreply.github.com>
2023-09-26 10:02:27 +0000

    メソッドに含める責任の範囲を変更

--- rails/app/models/city.rb
+++ rails/app/models/city.rb
@@ -4,12 +4,16 @@ class City < ApplicationRecord
     has_many :branches
     has_many :reviews
     has_many :assesments
-    def calculate_prefecture_branch_ids_duplicate_ids
+    def is_no_index
         prefecture_branch_ids = prefecture.branches.pluck(:id)
         city_branch_ids = branches.pluck(:id)
         duplicate_ids = prefecture_branch_ids & city_branch_ids
         prefecture_branch_ids_count = prefecture_branch_ids.count
         duplicate_ids_count = duplicate_ids.count
-        return { prefecture_branch_ids_count: prefecture_branch_ids_count, duplicate_ids_count: duplicate_ids_count}
+        if prefecture_branch_ids_count * 0.8 <= duplicate_ids_count
+            return true
+        else
+            return false
+        end
     end
 end
