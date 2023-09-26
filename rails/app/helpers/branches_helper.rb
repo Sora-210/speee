835cc05cf73d751e2f8d4ee4d249741a4c2d0dcb
Sora210 <46365745+Sora-210@users.noreply.github.com>
2023-09-26 02:27:24 +0000

    add: タイトルのprefixをつけるヘルパーを作成&実行

--- rails/app/helpers/branches_helper.rb
+++ rails/app/helpers/branches_helper.rb
@@ -1,2 +1,5 @@
 module BranchesHelper
+  def prefix_title_branch(branch)
+    "#{branch.company.name}#{branch.name}の不動産売却の評判は?"
+  end
 end
--- rails/app/helpers/review_helper.rb
+++ rails/app/helpers/review_helper.rb
@@ -1,10 +1,12 @@
 # app/helpers/reviews_helper.rb
 module ReviewHelper
+  def prefix_title_review(review)
+    "（#{review.created_at.strftime("%Y年%-m月")}投稿）口コミ・評判 | #{review.branch.company.name}#{review.branch.name}"
+  end
   def display_sale_reason(sale_reason_type)
     I18n.t("activerecord.attributes.review.sale_reasons.#{sale_reason_type}")
   end
-    def format_japanese_date(date)
-      date.strftime("%Y年%-m月")
-    end
+  def format_japanese_date(date)
+    date.strftime("%Y年%-m月")
   end
-  
+end
