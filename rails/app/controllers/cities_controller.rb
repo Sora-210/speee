0f71997dc2b1cf2d2277294fdf056c8201ca3c21
takano raika <62830507+Kissa0330@users.noreply.github.com>
2023-09-26 09:46:51 +0000

    リファクタリング

--- rails/app/controllers/cities_controller.rb
+++ rails/app/controllers/cities_controller.rb
@@ -3,15 +3,9 @@ class CitiesController < ApplicationController
     @is_no_index = false
     @prefecture = Prefecture.find_by(name: params[:prefecture_name])
     @city = City.find_by(name: params[:name])
-    if @city.blank?
-        render template: 'errors/404', status: 404, layout: 'error', content_type: 'text/html'
-        return 
-    end
+    return render template: 'errors/404', status: 404, layout: 'error', content_type: 'text/html' if @city.blank?
     @branches = Branch.where(city_id: @city.id)
-    if @branches.blank?
-        render template: 'errors/404', status: 404, layout: 'error', content_type: 'text/html'
-        return 
-    end
+    return render template: 'errors/404', status: 404, layout: 'error', content_type: 'text/html' if @branches.blank?
     duplication_count = @city.calculate_prefecture_branch_ids_duplicate_ids
     if duplication_count[:prefecture_branch_ids_count] * 0.8 <= duplication_count[:duplicate_ids_count]
       @is_no_index = true
