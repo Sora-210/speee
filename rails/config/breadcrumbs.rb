658381a8a346e3ce51544b76356e21924f0c49dc
Sora210 <46365745+Sora-210@users.noreply.github.com>
2023-09-26 08:00:46 +0000

    add: パンくずマップの作成

---
+++ rails/config/breadcrumbs.rb
@@ -0,0 +1,28 @@
+crumb :root do
+  link "Top"
+end
+
+crumb :prefecture do |prefecture|
+  link prefecture.name, prefecture_path(prefecture.name)
+  parent :root
+end
+
+crumb :city do |city|
+  link city.name, "/prefectures/#{city.prefecture.name}/cities/#{city.name}"
+  parent :prefecture, city.prefecture
+end
+
+crumb :company do |company|
+  link company.name
+  parent :root
+end
+
+crumb :branch do |branch|
+  link branch.name, branch_path(branch.id)
+  parent :company, branch.company
+end
+
+crumb :review do |review|
+  link "口コミ"
+  parent :branch, review.branch
+end
