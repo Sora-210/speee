d9d6ff58e46110731041622f804f067517b9e1d5
Sora210 <46365745+Sora-210@users.noreply.github.com>
2023-09-26 04:57:04 +0000

    add: 動的なタグをhelperで生成するように

--- rails/app/helpers/branches_helper.rb
+++ rails/app/helpers/branches_helper.rb
@@ -1,2 +1,8 @@
 module BranchesHelper
+    def create_description(branch)
+        "#{branch.company.name}#{branch.name}で実際に不動産売却や不動産購入をした人の口コミや評判を掲載しています！"
+    end
+    def create_keywords(branch)
+        "不動産会社,#{branch.company.name},#{branch.name.blank? ? "," : "#{branch.name}," }口コミ,評判"
+    end
 end
--- rails/app/helpers/review_helper.rb
+++ rails/app/helpers/review_helper.rb
@@ -1,10 +1,13 @@
 # app/helpers/reviews_helper.rb
 module ReviewHelper
+  def create_keywords(branch)
+    "不動産会社,#{branch.company.name},#{branch.name.blank? ? "," : "#{branch.name}," }口コミ,評判"
+  end
   def display_sale_reason(sale_reason_type)
     I18n.t("activerecord.attributes.review.sale_reasons.#{sale_reason_type}")
   end
-    def format_japanese_date(date)
-      date.strftime("%Y年%-m月")
-    end
+  def format_japanese_date(date)
+    date.strftime("%Y年%-m月")
   end
+end
   
