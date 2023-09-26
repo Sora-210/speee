566a1515decddad790d63637bbaba88834480e17
Sora210 <46365745+Sora-210@users.noreply.github.com>
2023-09-26 05:07:50 +0000

    change: 関数名変更 各ページごとにヘルパーを作成するので

--- rails/app/helpers/branches_helper.rb
+++ rails/app/helpers/branches_helper.rb
@@ -1,5 +1,5 @@
 module BranchesHelper
-  def prefix_title_branch(branch)
+  def create_title(branch)
     "#{branch.company.name}#{branch.name}の不動産売却の評判は?"
   end
 end
--- rails/app/helpers/review_helper.rb
+++ rails/app/helpers/review_helper.rb
@@ -1,6 +1,6 @@
 # app/helpers/reviews_helper.rb
 module ReviewHelper
-  def prefix_title_review(review)
+  def create_title(review)
     "（#{review.created_at.strftime("%Y年%-m月")}投稿）口コミ・評判 | #{review.branch.company.name}#{review.branch.name}"
   end
   def display_sale_reason(sale_reason_type)
