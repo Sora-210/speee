fceb0a3637a5f94042ed0fd8678bd1ff3e5011ab
takano raika <62830507+Kissa0330@users.noreply.github.com>
2023-09-26 05:40:20 +0000

    methodのprefixを削除

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-    rescue_from ActiveRecord::RecordNotFound,   with: :_render_404
+    rescue_from ActiveRecord::RecordNotFound,   with: :render_404
     private
-    def _render_404
+    def render_404
       if request.format.to_sym == :json
         render json: { error: "404 Not Found" }, status: :not_found
       else
