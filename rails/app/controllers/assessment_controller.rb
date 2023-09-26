19ac2fe50c2b8f70961e9f1bcfc6c00d735f1921
takano raika <62830507+Kissa0330@users.noreply.github.com>
2023-09-26 07:31:59 +0000

    i18nがバグってたので応急処置

--- rails/app/controllers/assessment_controller.rb
+++ rails/app/controllers/assessment_controller.rb
@@ -21,8 +21,9 @@ class AssessmentController < ApplicationController
          # TODO:ストロングパラメーターにリファクタリングを行う
        @assessment = Assesment.new(:city_id => @branch.city_id ,:prefecture_id => @branch.prefecture_id ,:branch_id => @branch.id, :last_name => params[:last_name], :first_name => params[:first_name], :last_name_kana => params[:last_name_kana], :first_name_kana => params[:first_name_kana], :tel => params[:phone], :email => params[:email], :address => params[:address], :building_type => params[:building_type].to_i, :exclusive_area => params[:exclusive_area].to_i, :land_area => params[:land_area].to_i, :building_area => params[:building_area].to_i, :room_plan_type => params[:room_plan_type].to_i, :constructed_year => params[:constructed_year].to_i)
         if @assessment.save
-          post_to_external_api_with_net_http(@assessment)
+          response = post_to_external_api_with_net_http(@assessment)
         else
+          return render :error
         end
       end
     end
