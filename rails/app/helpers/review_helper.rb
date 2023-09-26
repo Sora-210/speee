1d6d129bf64e20d037570184390e54f8afcc7e9a
Sora210 <46365745+Sora-210@users.noreply.github.com>
2023-09-26 05:08:48 +0000

    change: 既に関数が用意されていたので利用する形へ

--- rails/app/helpers/review_helper.rb
+++ rails/app/helpers/review_helper.rb
@@ -1,7 +1,7 @@
 # app/helpers/reviews_helper.rb
 module ReviewHelper
   def create_title(review)
-    "（#{review.created_at.strftime("%Y年%-m月")}投稿）口コミ・評判 | #{review.branch.company.name}#{review.branch.name}"
+    "（#{format_japanese_date(review.created_at)}投稿）口コミ・評判 | #{review.branch.company.name}#{review.branch.name}"
   end
   def display_sale_reason(sale_reason_type)
     I18n.t("activerecord.attributes.review.sale_reasons.#{sale_reason_type}")
