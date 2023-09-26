15ddeb6c1649a2e1d26ba4150bedc3a06bea3337
takano raika <62830507+Kissa0330@users.noreply.github.com>
2023-09-26 02:18:16 +0000

    都道府県が見つからない場合は404ページに飛ぶように

--- rails/app/controllers/prefectures_controller.rb
+++ rails/app/controllers/prefectures_controller.rb
@@ -1,7 +1,8 @@
 class PrefecturesController < ApplicationController
     def show
         @prefecture = Prefecture.find_by(name: params[:name])
-        if !@prefecture.present?
+        if @prefecture.blank?
+            render template: 'errors/404', status: 404, layout: 'error', content_type: 'text/html'
             return 
         end
             @branches = Branch.where(prefecture_id: @prefecture.id)
