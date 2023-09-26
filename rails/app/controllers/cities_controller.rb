51ee1b209c03759f2731d49c647ee6858e4f1482
takano raika <62830507+Kissa0330@users.noreply.github.com>
2023-09-26 05:38:06 +0000

    no_indexを動的に設定する wip

--- rails/app/controllers/cities_controller.rb
+++ rails/app/controllers/cities_controller.rb
@@ -1,12 +1,22 @@
 class CitiesController < ApplicationController
   def show
+    @is_no_index = false
     @prefecture = Prefecture.find_by(name: params[:prefecture_name])
     @city = City.find_by(name: params[:name])
     if @city.blank?
         render template: 'errors/404', status: 404, layout: 'error', content_type: 'text/html'
         return 
     end
-        @branches = Branch.where(city_id: @city.id)
+    @branches = Branch.where(city_id: @city.id)
+    if @branches.blank?
+        render template: 'errors/404', status: 404, layout: 'error', content_type: 'text/html'
+        return 
+    end
+    @prefecture_branch_ids = Branch.where(prefecture_id: @prefecture.id).pluck(:id)
+    @city_branch_ids = Branch.where(city_id: @city.id).pluck(:id)
+    @common_ids = @prefecture_branch_ids & @city_branch_ids
+    @common_ids_count = @common_ids.count
+    # 被り度が8割以上だったらフラグをtrueにするロジックを書く
 end
   end
   
