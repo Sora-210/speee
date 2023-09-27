07228e195aef0af89f1081270da4a1e6d7a4bdad
ryota108 <83957178+ryota108@users.noreply.github.com>
2023-09-27 06:35:38 +0000

    enumの修正

--- rails/app/models/assesment.rb
+++ rails/app/models/assesment.rb
@@ -5,21 +5,26 @@ class Assesment < ApplicationRecord
   belongs_to :prefecture
   belongs_to :city
 
-  enum :building_type, { apartment_house: 0, detached_house: 1, land: 2 }, prefix: true
+  enum building_type: {
+    'apartment_house' => 0,
+    'detached_house' => 1,
+    'land' => 2
+  }, _prefix: true
+
   enum room_plan_type: {
-    r1: 0,
-    k1: 1,
-    lk1: 2,
-    k2: 3,
-    lk2: 4,
-    k3: 5,
-    lk3: 6,
-    k4: 7,
-    lk4: 8,
-    k5: 9,
-    lk5: 10,
-    k6: 11,
-    lk6: 12,
-    over: 13
-  }, prefix: true
+    'r1' => 0,
+    'k1' => 1,
+    'lk1' => 2,
+    'k2' => 3,
+    'lk2' => 4,
+    'k3' => 5,
+    'lk3' => 6,
+    'k4' => 7,
+    'lk4' => 8,
+    'k5' => 9,
+    'lk5' => 10,
+    'k6' => 11,
+    'lk6' => 12,
+    'over' => 13
+  }, _prefix: true
 end
