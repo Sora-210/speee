d3baea64def5b9ff10d6f959547e7967d9d0f2f0
takano raika <62830507+Kissa0330@users.noreply.github.com>
2023-09-27 02:43:16 +0000

    validiateをmodelに移行

--- rails/app/controllers/assessment_controller.rb
+++ rails/app/controllers/assessment_controller.rb
@@ -15,15 +15,6 @@ class AssessmentController < ApplicationController
 
     return if @branch.nil?
 
-    unless validiate_phone(params)
-      @branch.errors.add(:phone, 'invalid phone number')
-      return render :index
-    end
-
-    unless validiate_email(params)
-      @branch.errors.add(:email, 'invalid email')
-      return render :index
-    end
     # TODO: ストロングパラメーターにリファクタリングを行う
     @assessment = Assesment.new(city_id: @branch.city_id, prefecture_id: @branch.prefecture_id,
                                 branch_id: @branch.id, last_name: params[:last_name], first_name: params[:first_name], last_name_kana: params[:last_name_kana], first_name_kana: params[:first_name_kana], tel: params[:phone], email: params[:email], address: params[:address], building_type: params[:building_type].to_i, exclusive_area: params[:exclusive_area].to_i, land_area: params[:land_area].to_i, building_area: params[:building_area].to_i, room_plan_type: params[:room_plan_type].to_i, constructed_year: params[:constructed_year].to_i)
@@ -35,25 +26,6 @@ class AssessmentController < ApplicationController
     redirect_to action: :error
   end
 end
-
-    private
-
-def validiate_email(params)
-  # TODO: モデル側の責務であるため、処理を移動する
-  return true if params[:email].match(/^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/)
-
-  false
-end
-
-def validiate_phone(params)
-  # TODO: モデル側の責務であるため、処理を移動する
-  if params[:phone].match(/\A(((0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1}|[5789]0[-(]?\d{4})[-)]?)|\d{1,4}-?)\d{4}|0120[-(]?\d{3}[-)]?\d{3})\z/)
-    return true
-  end
-
-  false
-end
-
     private
 
 def post_to_external_api_with_net_http(assessment)
--- rails/app/models/assesment.rb
+++ rails/app/models/assesment.rb
@@ -5,6 +5,10 @@ class Assesment < ApplicationRecord
   belongs_to :prefecture
   belongs_to :city
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  VALID_TEL_REGEX = /\A(((0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1}|[5789]0[-(]?\d{4})[-)]?)|\d{1,4}-?)\d{4}|0120[-(]?\d{3}[-)]?\d{3})\z/
+  validates :tel, presence: true, format: { with: VALID_TEL_REGEX }
   enum :building_type, { apartment_house: 0, detached_house: 1, land: 2 }, prefix: true
   enum :room_plan_type,
        { r1: 0, k1: 1, lk1: 2, k2: 3, lk2: 4, k3: 5, lk3: 6, k4: 7, lk4: 8, k5: 9, lk5: 10, k6: 11, lk6: 12, over: 13 }, prefix: true
