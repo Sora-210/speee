9b35698da0372897980344f4a5ee0afad97f25cf
takano raika <62830507+Kissa0330@users.noreply.github.com>
2023-09-28 02:41:23 +0000

    rubocop修正

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
       get 'thanks'
       get 'error'
     end
-  end 
+  end
   resources :prefectures, only: [:show], param: :name do
     resources :cities, only: [:show], param: :name
   end
